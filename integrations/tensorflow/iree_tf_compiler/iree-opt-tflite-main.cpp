2eca6c9dd5a5dfe09945e4143b20a40911a5f54a
Ben Vanik <benvanik@google.com>
2021-01-30 13:38:56 -0800

    Adding tflite import passes and metadata conversion. Progress on #3974. Unblocks #3972/#3975/#3978.

--- integrations/tensorflow/iree_tf_compiler/iree-opt-tflite-main.cpp
+++ integrations/tensorflow/iree_tf_compiler/iree-opt-tflite-main.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,29 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Main entry function for iree-tf-opt and derived binaries.
+// Main entry function for iree-tflite-opt and derived binaries.
 //
 // This is a bare-bones, minimal *-opt just for testing the handful of local
 // passes here. If you need something, add it, but add only what you need as
 // each addition will likely end up on the build critical path.
 
-#include "iree/tools/init_xla_dialects.h"
-#include "iree_tf_compiler/TF/Passes.h"
+#include "iree_tf_compiler/TFL/Passes.h"
 #include "llvm/Support/InitLLVM.h"
+#include "mlir/Dialect/Quant/QuantOps.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/Support/MlirOptMain.h"
-#include "tensorflow/compiler/mlir/tensorflow/dialect_registration.h"
+#include "tensorflow/compiler/mlir/lite/ir/tfl_ops.h"
+#include "tensorflow/compiler/mlir/tensorflow/ir/tf_ops.h"
 
 int main(int argc, char **argv) {
   llvm::InitLLVM y(argc, argv);
 
   mlir::DialectRegistry registry;
-  mlir::registerXLADialects(registry);
+  registry.insert<mlir::quant::QuantizationDialect>();
+  registry.insert<mlir::TF::TensorFlowDialect>();
+  registry.insert<mlir::TFL::TensorFlowLiteDialect>();
+  registry.insert<mlir::StandardOpsDialect>();
+  registry.insert<mlir::tosa::TosaDialect>();
 
-  mlir::RegisterAllTensorFlowDialects(registry);
-  mlir::iree_integrations::TF::registerAllDialects(registry);
+  mlir::iree_integrations::TFL::registerAllPasses();
 
-  if (failed(MlirOptMain(argc, argv, "IREE-TF modular optimizer driver\n",
+  if (failed(MlirOptMain(argc, argv, "IREE-TFL modular optimizer driver\n",
                          registry,
                          /*preloadDialectsInContext=*/false))) {
     return 1;
